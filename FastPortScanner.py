002961a11690ed16ef99a9069ed716a513a44e38
CODECRACKER-MAX <78782092+CODECRACKER-MAX@users.noreply.github.com>
2022-11-16 22:10:37 -0800

    Create FastPortScanner.py

---
+++ FastPortScanner.py
@@ -0,0 +1,32 @@
+import socket
+import time
+import threading
+global report
+report = []
+
+def Scan(port):
+    cache_res = ''
+    ip = '10.0.2.5'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    result = s.connect_ex((ip, port))
+    
+    if result == 0:
+        cache_res = '{} port is open!'.format(port)
+        report.append(cache_res)
+    s.close()
+
+start_time = time.time()
+
+'''
+for x in range(19, 25):
+    Scan(x)
+'''
+
+threads = [threading.Thread(target=Scan, args = (n,)) for n in range(1,65537)]
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+print(report)
+print("--------%.2fs Seconds ----------" %(time.time() - start_time))
+
